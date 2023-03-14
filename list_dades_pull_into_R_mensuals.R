d53f834f77df8454c395f7c1b46bdf1bbea90875
Wellington Migliari <49842956+wemigliari@users.noreply.github.com>
2023-03-14 22:01:40 +0100

    Add files via upload

---
+++ list_dades_pull_into_R_mensuals.R
@@ -0,0 +1,38 @@
+
+test_2019 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2019_Habitaclia_oferta/tractament_habit_oferta_2019.csv")
+test_2019$X <- NULL
+test_2020 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2020_Habitaclia_oferta/tractament_habit_oferta_2020.csv")
+test_2020$X <- NULL
+test_2021 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2021_Habitaclia_oferta/tractament_habit_oferta_2021.csv")
+test_2021$X <- NULL
+test_2022 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2022_Habitaclia_oferta/tractament_habit_oferta_2022.csv")
+test_2022$X <- NULL
+
+test_2019_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2019_Fotocasa_Oferta/tractament_Foto_oferta_2019.csv")
+#test_2019_f$X <- NULL
+test_2020_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2020_Fotocasa_Oferta/tractament_Foto_oferta_2020.csv")
+#test_2020_f$X <- NULL
+test_2021_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2021_Fotocasa_Oferta/tractament_Foto_oferta_2021.csv")
+#test_2021_f$X <- NULL
+test_2022_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2022_Fotocasa_Oferta/tractament_Foto_oferta_2022.csv")
+#test_2022_f$X <- NULL
+
+test_b2019 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2019_Habitaclia_oferta/tractament_habit_oferta_b2019.csv")
+test_b2019$X <- NULL
+test_b2020 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2020_Habitaclia_oferta/tractament_habit_oferta_b2020.csv")
+test_b2020$X <- NULL
+test_b2021 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2021_Habitaclia_oferta/tractament_habit_oferta_b2021.csv")
+test_b2021$X <- NULL
+test_b2022 <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2022_Habitaclia_oferta/tractament_habit_oferta_b2022.csv")
+test_b2022$X <- NULL
+
+test_b2019_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2019_Fotocasa_Oferta/tractament_Foto_oferta_b2019.csv")
+test_b2019_f$X <- NULL
+test_b2020_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2020_Fotocasa_Oferta/tractament_Foto_oferta_b2020.csv")
+test_b2020_f$X <- NULL
+test_b2021_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2021_Fotocasa_Oferta/tractament_Foto_oferta_b2021.csv")
+test_b2021_f$X <- NULL
+test_b2022_f <- read.csv("/Users/wemigliari/Documents/Pós-Doutorado & Doutorado/Pós-Doc/Observatori_Metropolita/Dades/2022_Fotocasa_Oferta/tractament_Foto_oferta_b2022.csv")
+test_b2022_f$X <- NULL    
+
+
